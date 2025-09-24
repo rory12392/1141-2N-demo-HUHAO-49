80fee7d56dfae75607cdf43a0bde57b0933610cf
rory12392 <rory9256@gmail.com>
2025-09-24 20:21:03 +0800

    W02-P1: App_xx with 6 books

---
+++ demo/md/w02-booklist-49/w02-booklist-49.md
@@ -0,0 +1,21 @@
+[Github URL](https://github.com/rory12392/1141-2N-demo-HUHAO-49)
+
+### W02-P1: App_xx with 6 books
+
+#### => Chrome demo
+
+![](w02-p1-1.png)
+
+#### => code demo
+
+![](w02-p1-2.png)
+
+![](w02-p1-3.png)
+
+```
+
+```
+
+### W01-logs: git logs of W01
+
+![](w02-logs.png)
---
+++ demo/w02_booklist_49/src/booklist_data2.js
@@ -0,0 +1,42 @@
+const books_data = [
+  {
+    id: 1,
+    img: 'https://m.media-amazon.com/images/I/61eVHPwK-xL._SY342_.jpg',
+    title: '(1) The Surrogate Mother: An addictive psychological thriller',
+    author: 'Kindle Edition',
+  },
+  {
+    id: 2,
+    img: 'https://m.media-amazon.com/images/I/71pGYKl2rvL._SL1500_.jpg',
+    title:
+      '(2) Stop, in the Name of God: Why Honoring the Sabbath Will Transform Your Life',
+    author: 'Charlie Kirk',
+  },
+  {
+    id: 3,
+    img: 'https://m.media-amazon.com/images/I/61urHRFZpYL._SL1500_.jpg',
+    title: '(3) 107 Days',
+    author: 'Kamala Harris',
+  },
+  {
+    id: 4,
+    img: 'https://m.media-amazon.com/images/I/81AHTyq2wVL._SY342_.jpg',
+    title: '(4) The Housemaid',
+    author: 'Freida McFadden',
+  },
+  {
+    id: 5,
+    img: 'https://m.media-amazon.com/images/I/8150S2FjgIL._SY342_.jpg',
+    title: '(5) King of Ashes: A Novel',
+    author: 'S. A. Cosby',
+  },
+  {
+    id: 6,
+    img: 'https://m.media-amazon.com/images/I/7146NGo5BLL._SY342_.jpg',
+    title:
+      '(6) Adrift: A True Story of Tragedy on the Icy Atlantic and the One Who Lived to Tell about It',
+    author: 'Brian Murphy, Toula Vlahou ',
+  },
+];
+
+export default books_data;
