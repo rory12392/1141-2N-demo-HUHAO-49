6d4f960476ab7708d258679dd13d84772f880492
rory12392 <rory9256@gmail.com>
2025-10-01 19:19:58 +0800

    W03-P1: Create router in App_xx.jsx

---
+++ demo/w03_blogs_49/src/pages/HomeLayoutPage_49.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom';
+
+const HomeLayoutPage_49 = () => {
+  return (
+    <>
+      {/* <Navbar /> */}
+      <Outlet />
+    </>
+  );
+};
+
+export default HomeLayoutPage_49;
---
+++ demo/w03_blogs_49/src/pages/index.jsx
@@ -0,0 +1,3 @@
+export { default as HomeLayoutPage_49 } from './HomeLayoutPage_49';
+export { default as HomePage_49 } from './HomePage_49';
+export { default as BlogStaticPage_49 } from './BlogStaticPage_49';
